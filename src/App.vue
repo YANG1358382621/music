25b1b3045901111cee3ff83a96335be3f189a554
YANG1358382621 <1358382621@qq.com>
2023-06-30 16:18:10 +0800

    fist commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <meting-js
+      id="8856355917"
+      server="tencent"
+      type="playlist"
+      fixed=""
+      autoplay="true"
+      loop="all"
+      order="list"
+      preload="auto"
+      list-folded="false"
+      list-max-height=""
+      lrc-type=""
+    >
+    </meting-js>
+  </div>
+</template>
+
+<style lang="less"></style>
